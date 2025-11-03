a73a565fb3e855d0d827b5ee1e587eeb326a9238
vasujain275 <vasujain275@gmail.com>
2025-11-03 23:22:51 +0530

    feat(bot): add set_bot_commands_menu helper

---
+++ app/bot/menu.py
@@ -0,0 +1,14 @@
+from aiogram import Bot
+from aiogram.types import BotCommand
+
+async def set_bot_commands_menu(my_bot: Bot) -> None:
+    # Register commands for Telegram bot (menu)
+    commands = [
+        BotCommand(command="/id", description="👋 Get my ID"),
+        BotCommand(command="/start",description="Start Chatting!")
+    ]
+    try:
+        await my_bot.set_my_commands(commands)
+    except Exception as e:
+        # logger.error(f"Can't set commands - {e}")
+        print(e)
